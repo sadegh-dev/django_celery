92068f7a7ba5473c3a0a1701f14456a8e9e510ef
sadegh-dev <sadegh.rez@gmail.com>
2022-04-18 09:35:23 +0430

    delete office app , start again

--- core/core/celery.py
+++ core/core/celery.py
@@ -7,4 +7,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 celery_app = Celery('core')
 celery_app.config_from_object('django.conf:settings', namespace='CELERY')
 celery_app.autodiscover_tasks()
-315
